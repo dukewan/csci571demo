7397959c20078efd6d82b477eb866ccd67e90ebf
yizhaohe <heyizhaobupt@gmail.com>
2016-02-02 18:12:38 -0800

    update server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World CSCI571!\n');
-}).listen(process.env.PORT, process.env.IP, function () {
-  console.log(`Server running at http://${process.env.IP}:${process.env.PORT}/`);
+}).listen('127.0.0.1', '45678', function () {
+  console.log(`Server running at port: 45678`);
 });

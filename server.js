89dc06c58b40acb4f1dc4d94b9ce9fbfba2edeb6
yizhaohe <heyizhaobupt@gmail.com>
2016-02-02 18:17:02 -0800

    update

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World CSCI571!\n');
 }).listen(hostname, port, function () {
-  console.log('Server running at http://${hostname}:${port}/');
+  console.log('Server running at http://' + hostname + ':' + port +'/');
 });

760180a41225de9d5ee13a33cea26af826b879b5
yizhaohe <heyizhaobupt@gmail.com>
2016-02-02 18:27:02 -0800

    update

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
-var hostname = '127.0.0.1';
+var hostname = '52.79.54.82';
 var port = 45678;
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World CSCI571!\n');
-}).listen();
+}).listen(hostname, port, function () {
+  console.log('Server running at http://' + hostname + ':' + port +'/');
+});
